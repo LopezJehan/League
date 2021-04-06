10d7e2e772e72955bd85a5a0b85a170f4c744d54
Jehan Lopez <jiwan33@hotmail.fr>
2021-04-06 23:19:40 +0200

    Some tests for next graph

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -17,3 +17,16 @@ data_LEC <- all_data %>%
   mutate(event = ifelse(playoffs == 1, paste0(split, " playoffs"), split))
 
 # data_LFL <- all_data %>% filter(league == "LFL")
+
+##### Stats #####
+
+stats_LEC_players <- data_LEC %>% 
+  group_by(player, team) %>% 
+  summarise(
+    kills = sum(kills),
+    deaths = sum(deaths),
+    assists = sum(assists)) %>% 
+  mutate(kda = calculate_kda(kills, deaths, assists))
+
+stats_LEC_teams <- stats_LEC_players[stats_LEC_players$player == "",]
+stats_LEC_players <- stats_LEC_players[stats_LEC_players$player != "",]
