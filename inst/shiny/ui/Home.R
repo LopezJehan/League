4f06e02e3f36c7f8ef236a2da916f7d10bb7a6a5
Jehan Lopez <jiwan33@hotmail.fr>
2021-04-24 22:27:26 +0200

    Added some text and images to the homepage

--- inst/shiny/ui/Home.R
+++ inst/shiny/ui/Home.R
@@ -1,3 +1,5 @@
 tabItem(tabName = "Home",
         HTML("<h1><center>WELCOME TO MY LEAGUE OF LEGENDS APPLICATION</center></h1>"),
-        HTML("<center><img src=LOL.png width=1000></img></center>"))
+        HTML("<center><img src=LOL.png width=1000></img></center>"),
+        br(),
+        HTML("<center><h2>In this app, you can view statistics for three leagues around the world: <img src=LCK.png height=50></img>LCK, <img src=LPL.png height=50></img>LPL and <img src=LEC.png height=50></img>LEC.</h2></center>"))
