36d263d3b1d6acfd4ef45852d1d79542431c591a
Jehan Lopez <jiwan33@hotmail.fr>
2021-05-14 11:47:47 +0200

    Added logos in picker input for LEC matches

--- inst/shiny/server/LEC/LEC_match.R
+++ inst/shiny/server/LEC/LEC_match.R
@@ -14,16 +14,42 @@ matchs_LEC <- data.frame(blue_teams_LEC, red_teams_LEC) %>%
          "Red" = team.1,
          "Blue_result" = result,
          "Red_result" = result.1) %>% 
-  mutate(row = 1:nrow(matchs_LEC),
-         Result = paste(row, "-", Blue, Blue_result, "-", Red_result, Red))
+  mutate(row = 1:nrow(blue_teams_LEC),
+         Result = paste(row, "-", Blue, Blue_result,
+                        "-", Red_result, Red))
 
+flags_LEC_simplified <- flags_LEC %>% select(team, img)
+
+matchs_LEC <- merge(matchs_LEC, flags_LEC_simplified,
+                    by.x = "Blue", by.y = "team") %>% 
+  rename("Blue_img" = img)
+
+matchs_LEC <- merge(matchs_LEC, flags_LEC_simplified,
+                    by.x = "Red", by.y = "team") %>% 
+  rename("Red_img" = img) %>% 
+  arrange(row)
+
+# Picker Input
 output$LEC_Match_picker <- renderUI(
   pickerInput("LEC_match_picker",
               "Choose a match",
               choices = matchs_LEC$Result,
-              selected = NULL,
-              multiple = TRUE,
-              options = pickerOptions(maxOptions = 1))
+              choicesOpt = list(content =  
+                                  lapply(matchs_LEC$Result, FUN = function(result) {
+                                    line <- matchs_LEC[matchs_LEC$Result == result,]
+                                    result_break <- unlist(strsplit(result, " - "))
+                                    HTML(paste0(
+                                      tags$b(result_break[1]),
+                                      tags$b(" - "),
+                                      tags$img(src = line$Blue_img),
+                                      tags$b(result_break[2]),
+                                      tags$b(" - "),
+                                      tags$b(result_break[3]),
+                                      tags$img(src = line$Red_img)
+                                    ))
+                                  })
+              )
+  )
 )
 
 # Display table
@@ -31,7 +57,7 @@ output$LEC_matches <- DT::renderDataTable({
   if(!is.null(input$LEC_match_picker)){
     res_table <- buildTable(data = data_LEC,
                             number = as.numeric(strsplit(input$LEC_match_picker, " - ")[[1]][1]))
-
+    
     DT::datatable(
       res_table[[1]],
       options = list(dom = 't',
