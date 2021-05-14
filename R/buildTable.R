32f6b141d52b88b8d53435c3d0a4cfea33527d41
Jehan Lopez <jiwan33@hotmail.fr>
2021-05-15 00:28:14 +0200

    Separated image from champ

--- R/buildTable.R
+++ R/buildTable.R
@@ -18,21 +18,21 @@ buildTable <- function(number, data){
   colonne2 <- c(team1$champion)
   colonne2_new <- c()
   for(elt in colonne2){
-    colonne2_new <- c(colonne2_new, paste(elt, paste0('<img src="perso/', elt, '.png" height="30"></img>')))
+    colonne2_new <- c(colonne2_new, paste0('<img src="perso/', elt, '.png" height="30"></img>'))
   }
   
   colonne5 <- c(team2$champion)
   colonne6 <- c(team2$player)
   colonne5_new <- c()
   for(elt in colonne5){
-    colonne5_new <- c(colonne5_new, paste(paste0('<img src="perso/', elt, '.png" height="30"></img>'), elt))
+    colonne5_new <- c(colonne5_new, paste0('<img src="perso/', elt, '.png" height="30"></img>'))
   }
   
   teamskda <- paste(teams$kills, teams$deaths, teams$assists, sep = " - ")
   colonne3 <- paste(team1$kills, team1$deaths, team1$assists, sep = " - ")
   colonne4 <- paste(team2$kills, team2$deaths, team2$assists, sep = " - ")
   
-  tab <- data.frame(colonne1, colonne2_new, colonne3, colonne4, colonne5_new, colonne6)
-  colnames(tab) <- c("Blue Side", teams$team[1], teamskda[1], teamskda[2], teams$team[2], "Red Side")
+  tab <- data.frame(colonne1, colonne2, colonne2_new, colonne3, colonne4, colonne5_new, colonne5, colonne6)
+  colnames(tab) <- c(rep(teams$team[1],3), teamskda[1], teamskda[2], rep(teams$team[2],3))
   return(list(tab[1:5,], title))
 }
