6f35adbceaecd604a24291c8a7a76962a6c01b05
Jehan Lopez <jiwan33@hotmail.fr>
2021-07-04 22:55:59 +0200

    Added LFL to the home page

--- inst/shiny/ui/Home.R
+++ inst/shiny/ui/Home.R
@@ -2,4 +2,4 @@ tabItem(tabName = "Home",
         HTML("<h1><center>WELCOME TO MY LEAGUE OF LEGENDS APPLICATION</center></h1>"),
         HTML("<center><img src=LOL.png width=1000></img></center>"),
         br(),
-        HTML("<center><h2>In this app, you can view statistics for three leagues around the world: <img src=LCK.png height=50></img>LCK, <img src=LPL.png height=50></img>LPL and <img src=LEC.png height=50></img>LEC.</h2></center>"))
+        HTML("<center><h2>In this app, you can view statistics for three leagues around the world: <img src=LCK.png height=50></img>LCK, <img src=LPL.png height=50></img>LPL, <img src=LEC.png height=50></img>LEC and <img src=LFL.png height=50></img>LFL.</h2></center>"))
