71cc2fe8ef2cf9395d300bc0c0c335c1893160f2
Jehan Lopez <jiwan33@hotmail.fr>
2021-08-23 19:46:01 +0200

    Added legal boilerplate at the bottom of the Home

--- inst/shiny/ui/Home.R
+++ inst/shiny/ui/Home.R
@@ -2,4 +2,8 @@ tabItem(tabName = "Home",
         HTML("<h1><center>WELCOME TO MY LEAGUE OF LEGENDS APPLICATION</center></h1>"),
         HTML("<center><img src=LOL.png width=1000></img></center>"),
         br(),
-        HTML("<center><h2>In this app, you can view statistics for three leagues around the world: <img src=LCK.png height=50></img>LCK, <img src=LPL.png height=50></img>LPL, <img src=LEC.png height=50></img>LEC and <img src=LFL.png height=50></img>LFL.</h2></center>"))
+        HTML("<center><h2>In this app, you can view statistics for three leagues around the world: <img src=LCK.png height=50></img>LCK, <img src=LPL.png height=50></img>LPL, <img src=LEC.png height=50></img>LEC and <img src=LFL.png height=50></img>LFL.</h2></center>"),
+        br(),
+        br(),
+        br(),
+        HTML("<center><h5>This app isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.</h5></center>"))
