e91e3a61c9ae358e7794a9a7de61ea88b8334349
Jehan Lopez <jiwan33@hotmail.fr>
2021-10-20 23:22:20 +0200

    Updated data, global.R and Desc

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -1,7 +1,7 @@
 library(League)
 
 ##### Original data #####
-# all_data <- as.data.frame(read.csv("inst/shiny/data/2021_data.csv"))
+# all_data <- fread("inst/shiny/data/2021_data.csv")
 # save(all_data, file = "inst/shiny/data/all_data.RData")
 load("data/all_data.RData")
 
@@ -20,4 +20,9 @@ data_LEC <- all_data %>%
 
 data_LFL <- all_data %>% 
   filter(league == "LFL") %>% 
-  mutate(event = ifelse(playoffs == 1, paste0(split, " playoffs"), split))
+  mutate(event = ifelse(playoffs == 1, paste0(split, " playoffs"), split)) %>% 
+  mutate(event = ifelse(event == "", "Up & Down", event)) %>% 
+  mutate(event = ifelse(event == " playoffs", "Finals", event))
+
+data_Worlds <- all_data %>% 
+  filter(league == "WCS")
